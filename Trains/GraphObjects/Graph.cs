705a6cc489fbae70aabe28425dc3f0adfeba7a76
Rodrigo Valladares Cotta <rodrigo__vc@hotmail.com>
2016-08-25 00:36:54 -0300

    Add node comparer tests

---
+++ Trains/GraphObjects/Graph.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Trains.GraphObjects;
+
+namespace Trains.Graph
+{
+    public class Graph
+    {
+        HashSet<Node> nodes = new HashSet<Node>();
+    }
+}
