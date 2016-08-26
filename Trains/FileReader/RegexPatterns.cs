8a84fa12cb28a088f95efb4d12f2dbc8ef41fab1
Rodrigo Valladares Cotta <rodrigo__vc@hotmail.com>
2016-08-26 09:54:10 -0300

    Regex Finlized

--- Trains/FileReader/RegexPatterns.cs
+++ Trains/FileReader/RegexPatterns.cs
@@ -11,22 +11,31 @@ namespace Trains.FileReader
 
     public static class RegexPatterns
     {
-        const string DistanceOfRoutePattern = "(?:^Thedistanceoftheroute)(?:([A-Z])-)+(?:([A-z]).$)";
-        const string MaxNumberOfStops = "(?:^Thenumberoftripsstartingat)([A-Z])(?:andendingat)([A-Z])(?:withamaximumof([0-9]+)stops.$)";
-        const string ExactNumberOfStops = "(?:^Thenumberoftripsstartingat)([A-Z])(?:andendingat)([A-Z])(?:withexactly([0-9]+)stops.$)";
+        const string DistanceOfRoutePattern = @"(?:^Thedistanceoftheroute)(?:([A-Z])-)+(?:([A-z]).$)";
+        const string MaxNumberOfStops = @"(?:^Thenumberoftripsstartingat)([A-Z])(?:andendingat)([A-Z])(?:withamaximumof([0-9]+)stops.$)";
+        const string ExactNumberOfStops = @"(?:^Thenumberoftripsstartingat)([A-Z])(?:andendingat)([A-Z])(?:withexactly([0-9]+)stops.$)";
+        const string ShortestPath = @"(?:^Thelengthoftheshortestroute\(intermsofdistancetotravel\)from)([A-Z])(?:to)([A-Z])(?:.$)";
+        const string RoutesSmallerThan = @"(?:^Thenumberofdifferentroutesfrom)([A-Z])(?:to)([A-Z])(?:withadistanceoflessthan)([0-9]+)(?:.$)";
+        const string Graph = @"(?:^Graph:)(?:(?:(?:([A-Z]{2}[0-9]+)(?:,))*([A-Z]{2}[0-9]+)(?:$))|(?:$))";
         public static ImmutableDictionary<LineType, string> Patterns = ImmutableDictionary.CreateBuilder<LineType, string>()
-            .AddFluent(LineType.DistanceOfRoutePattern, DistanceOfRoutePattern)
+            .AddFluent(LineType.DistanceOfRoute, DistanceOfRoutePattern)
             .AddFluent(LineType.MaxNumberOfStops, MaxNumberOfStops)
             .AddFluent(LineType.ExactNumberOfStops, ExactNumberOfStops)
+            .AddFluent(LineType.ShortestPath, ShortestPath)
+            .AddFluent(LineType.RoutesSmallerThan, RoutesSmallerThan)
+            .AddFluent(LineType.Graph, Graph)
             .ToImmutable();
 
     }
 
     public enum LineType
     {
-        DistanceOfRoutePattern,
+        DistanceOfRoute,
         MaxNumberOfStops,
         ExactNumberOfStops,
+        ShortestPath,
+        RoutesSmallerThan,
+        Graph,
         InvalidLine
     }
 }
--- TrainsTest/RegexPatternsTest.cs
+++ TrainsTest/RegexPatternsTest.cs
@@ -27,7 +27,7 @@ namespace TrainsTest
         [TestMethod]
         public void DistanceOfRoutePatternTest()
         {
-            Func<List<string>,TokenizedLine> expectedResult = a => new TokenizedLine(a, LineType.DistanceOfRoutePattern);
+            Func<List<string>,TokenizedLine> expectedResult = a => new TokenizedLine(a, LineType.DistanceOfRoute);
             (new Tuple<string, TokenizedLine>[] {
             Tuple.Create("The distance of the route A-B- C.", expectedResult(new List<string>() { "A","B","C" })),
             Tuple.Create("The distance of the route A - D - C.", expectedResult(new List<string>() { "A","D","C" })),
@@ -43,6 +43,8 @@ namespace TrainsTest
             var expectedResult = new TokenizedLine(new List<string>(), LineType.InvalidLine);
             (new Tuple<string, TokenizedLine>[] {
             Tuple.Create("The distance of the route A-B- C", expectedResult),
+            Tuple.Create("The distance of the route A.", expectedResult),
+            Tuple.Create("The distance of the route.", expectedResult),
             Tuple.Create("The distance of the route ADFDFD - D - C.", expectedResult),
             Tuple.Create("The distance of the sdfa route A - D.", expectedResult),
             Tuple.Create("The distance of the route A - 3 - 8 - C - D.", expectedResult) })
@@ -50,7 +52,7 @@ namespace TrainsTest
         }
 
         [TestMethod]
-        public void MaxNumberOfStopsTest()
+        public void MaxNumberOfStopsPatternTest()
         {
             Func<List<string>, TokenizedLine> expectedResult = a => new TokenizedLine(a, LineType.MaxNumberOfStops);
             (new Tuple<string, TokenizedLine>[] {
@@ -59,11 +61,22 @@ namespace TrainsTest
             Tuple.Create("The number of trips starting at C and ending at B with a maximum of 55 stops.", 
             expectedResult(new List<string>() { "C","B","55" })),
             Tuple.Create("The number of trips starting at A and ending at B with a maximum of 150 stops.", 
-            expectedResult(new List<string>() { "A","B","150" }))}).ForEach(l => assertPattern(l.Item1, l.Item2));
+            expectedResult(new List<string>() { "A","B","150" }))})
+            .ForEach(l => assertPattern(l.Item1, l.Item2));
         }
 
         [TestMethod]
-        public void ExactNumberOfStopsTest()
+        public void MaxNumberOfStopsPatternTestFailure()
+        {
+            var expectedResult = new TokenizedLine(new List<string>(), LineType.InvalidLine);
+            (new Tuple<string, TokenizedLine>[] {
+            Tuple.Create("The number of trips starting at C and ending at CA with a maximum of 3 stops.", expectedResult),
+            Tuple.Create("The number of trips stating at C and ending at B with a maximum of 55 stops.", expectedResult)})
+            .ForEach(l => assertPattern(l.Item1, l.Item2));
+        }
+
+        [TestMethod]
+        public void ExactNumberOfStopsPatternTest()
         {
             Func<List<string>, TokenizedLine> expectedResult = a => new TokenizedLine(a, LineType.ExactNumberOfStops);
             (new Tuple<string, TokenizedLine>[] {
@@ -72,22 +85,94 @@ namespace TrainsTest
             Tuple.Create("The number of trips starting at D and ending at E with exactly 45 stops.",
             expectedResult(new List<string>() { "D","E","45" })),
             Tuple.Create("The number of trips starting at B and ending at B with exactly 88 stops.",
-            expectedResult(new List<string>() { "B","B","88" }))}).ForEach(l => assertPattern(l.Item1, l.Item2));
+            expectedResult(new List<string>() { "B","B","88" }))})
+            .ForEach(l => assertPattern(l.Item1, l.Item2));
         }
 
+        [TestMethod]
+        public void ExactNumberOfStopsPatternTestFailure()
+        {
+            var expectedResult = new TokenizedLine(new List<string>(), LineType.InvalidLine);
+            (new Tuple<string, TokenizedLine>[] {
+            Tuple.Create("The number of trips starting at AB and ending at C with exactly 7 stops.", expectedResult),
+            Tuple.Create("The number of trips starting at D and ending at E with exatly 45 stops.", expectedResult)})
+            .ForEach(l => assertPattern(l.Item1, l.Item2));
+        }
 
-        /*[TestMethod]
-        public void EqualityTestSuccess()
-       {
-            var id = Guid.NewGuid();
-            var node1 = new Node() { Id = id };
-            var node2 = new Node() { Id = id };
-            var node3 = node1;
-            var nodecmp = new NodeComparer();
-            Assert.IsTrue(nodecmp.Equals(node1, node2));
-            Assert.IsTrue(nodecmp.Equals(node2, node3));
-            Assert.IsTrue(nodecmp.Equals(node2, node3));
-        }*/
+        [TestMethod]
+        public void ShortestPathPatternTest()
+        {
+            Func<List<string>, TokenizedLine> expectedResult = a => new TokenizedLine(a, LineType.ShortestPath);
+            (new Tuple<string, TokenizedLine>[] {
+            Tuple.Create("The length of the shortest route (in terms of distance to travel) from A to C.",
+            expectedResult(new List<string>() { "A","C" })),
+            Tuple.Create("The length of the shortest route (in terms of distance to travel) from B to B.",
+            expectedResult(new List<string>() { "B","B" })),
+            Tuple.Create("The length of the shortest route (in terms of distance to travel) from E to A.",
+            expectedResult(new List<string>() { "E","A" }))})
+            .ForEach(l => assertPattern(l.Item1, l.Item2));
+        }
+
+        [TestMethod]
+        public void ShortestPathPatternTestFailure()
+        {
+            var expectedResult = new TokenizedLine(new List<string>(), LineType.InvalidLine);
+            (new Tuple<string, TokenizedLine>[] {
+            Tuple.Create("The length of the shortest route (in terms of distance to travel) from AS to CX.", expectedResult),
+            Tuple.Create("The length of the shortest roue (in terms of distance to travel) from A to C.", expectedResult)})
+            .ForEach(l => assertPattern(l.Item1, l.Item2));
+        }
+
+        [TestMethod]
+        public void RoutesSmallerThanPatternTest()
+        {
+            Func<List<string>, TokenizedLine> expectedResult = a => new TokenizedLine(a, LineType.RoutesSmallerThan);
+            (new Tuple<string, TokenizedLine>[] {
+            Tuple.Create("The number of different routes from C to C with a distance of less than 30.",
+            expectedResult(new List<string>() { "C","C","30" })),
+            Tuple.Create("The number of different routes from A to E with a distance of less than 555555555.",
+            expectedResult(new List<string>() { "A","E", "555555555" })),
+            Tuple.Create("The number of different routes from D to B with a distance of less than 456.",
+            expectedResult(new List<string>() { "D","B","456" }))})
+            .ForEach(l => assertPattern(l.Item1, l.Item2));
+        }
+
+        [TestMethod]
+        public void RoutesSmallerThanPatternTestFailure()
+        {
+            var expectedResult = new TokenizedLine(new List<string>(), LineType.InvalidLine);
+            (new Tuple<string, TokenizedLine>[] {
+            Tuple.Create("The number of different routes from C to C with a distance of less than 30A.", expectedResult),
+            Tuple.Create("The number of different routes from CV to C with a distance of less than 30.", expectedResult)})
+            .ForEach(l => assertPattern(l.Item1, l.Item2));
+        }
+
+        [TestMethod]
+        public void GraphPatternTest()
+        {
+            Func<List<string>, TokenizedLine> expectedResult = a => new TokenizedLine(a, LineType.Graph);
+            (new Tuple<string, TokenizedLine>[] {
+            Tuple.Create("Graph: AB5, BC4, CD8, DC88, DE6, AD5, CE2, EB3, AE7",
+            expectedResult(new List<string>() { "AB5", "BC4", "CD8", "DC88", "DE6", "AD5", "CE2", "EB3", "AE7" })),
+            Tuple.Create("Graph: AB57", expectedResult(new List<string>() { "AB57"})),
+            Tuple.Create("Graph: ", expectedResult(new List<string>())),
+            Tuple.Create("Graph: AD5, CE2, EB3, AE766",
+            expectedResult(new List<string>() { "AD5", "CE2", "EB3", "AE766" }))})
+            .ForEach(l => assertPattern(l.Item1, l.Item2));
+        }
+
+        [TestMethod]
+        public void GraphPatternTestFailure()
+        {
+            var expectedResult = new TokenizedLine(new List<string>(), LineType.InvalidLine);
+            (new Tuple<string, TokenizedLine>[] {
+            Tuple.Create("Graph: AB5, BC4, CD8, DC8, DE6, AD5, CE2 EB3, AE7", expectedResult),
+            Tuple.Create("Graph: AB5,", expectedResult),
+            Tuple.Create("Grah: ", expectedResult),
+            Tuple.Create("Graph: AD5, CE2, EAB3, AE7", expectedResult)})
+            .ForEach(l => assertPattern(l.Item1, l.Item2));
+        }
+
 
 
     }
